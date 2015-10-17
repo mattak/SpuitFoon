f346abc61a8452a4e0e49a0dd0cbc9893313a8a7
Takuma Maruyama <maruyama@mfac.jp>
2015-10-17 16:24:09 +0900

    design(Main): put designs

---
+++ Assets/Resources/Scripts/Util/TouchSpawnBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+
+public class TouchSpawnBehaviour : MonoBehaviour {
+	public Button button;
+
+	// Use this for initialization
+	void Start () {
+		button.OnClickAsObservable ()
+			.Subscribe (thisUnit => {
+				Debug.Log ("Clicked");
+			});
+	}
+}
