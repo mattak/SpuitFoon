e25de2891f097bb9065bb4c68ef2821271d3080a
Takuma Maruyama <maruyama@mfac.jp>
2015-10-17 15:11:33 +0900

    feat(Start): move to main scene

---
+++ Assets/Resources/Scripts/ApplicationLoadBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplicationLoadBehaviour : MonoBehaviour {
+	public string nextScene = "Main";
+
+	// Use this for initialization
+	public void LoadScene () {
+		Application.LoadLevel (nextScene);
+	}
+}
