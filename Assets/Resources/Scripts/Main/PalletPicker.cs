a95e9f9e0ecaddf6c68167c4cd852d46ea9ee91a
mattak <mattak.me@gmail.com>
2015-10-18 10:46:26 +0900

    feat(Main): add button behaviour

---
+++ Assets/Resources/Scripts/Main/PalletPicker.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UniRx.Triggers;
+
+public class PalletPicker : MonoBehaviour {
+	public Team team = Team.Player1;
+	public Button[] buttons;
+	private string[] placement;
+
+	// Use this for initialization
+	void Start () {
+		placement = new string[buttons.Length];
+
+		for (int i = 0; i < buttons.Length; i++) {
+			int index = i;
+			placement[index] = null;
+
+			// button behaves
+			buttons[index].OnClickAsObservable().Subscribe(_ => {
+				placement[index] = null;
+				UpdatePicker();
+			});
+		}
+
+		UpdatePicker ();
+	}
+
+	public void UpdatePicker() {
+		System.Random random = new System.Random ();
+		for (int i = 0; i < buttons.Length; i++) {
+			if (placement[i] == null) {
+				Partner partner = PartnerExt.Random ();
+				string path = partner.SpritePath (team);
+				placement[i] = path;
+
+				Image image = buttons[i].transform.GetChild (0).GetComponent<Image>();
+				image.sprite = Resources.Load<Sprite>(path);
+
+				Debug.Log ("pickup: " + partner);
+			}
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/Main/Partner/Partner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Partner {
+	Fudo,
+	Pyon,
+	Hasir,
+}
+
+static class PartnerExt {
+	public static string SpritePath(this Partner partner, Team team) {
+		string[] pathes = {
+			"Sprites/foon_orange_fudo",
+			"Sprites/foon_orange_pyon",
+			"Sprites/foon_orange_hasir",
+			"Sprites/foon_purple_fudo",
+			"Sprites/foon_purple_pyon",
+			"Sprites/foon_purple_hasir"
+		};
+
+		if (team == Team.Player1) {
+			return pathes [(int)partner];
+		} else if (team == Team.Player2) {
+			return pathes [(int)partner + 3]; // TODO: hard cording fix
+		}
+
+		return null;
+	}
+
+	public static Partner Random() {
+		System.Random random = new System.Random ();
+		int index = random.Next (3); // TODO: fix hard cording.
+		return (Partner)System.Enum.ToObject(typeof(Partner), index);
+	}
+}
