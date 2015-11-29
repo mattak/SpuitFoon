1863e47df74de01ed7973a09929cf0b282438247
Takuma Maruyama <maruyama@mfac.jp>
2015-11-29 16:59:54 +0900

    fix(PalletPicker): pallet picker code

--- Assets/Resources/Scripts/Main/PalletPicker.cs
+++ Assets/Resources/Scripts/Main/PalletPicker.cs
@@ -4,84 +4,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UniRx;
 using UniRx.Triggers;
+using ViewModel;
 
-public class PalletPicker : MonoBehaviour {
-	public Team team = Team.Player1;
-	// public Button[] buttons;
-	public Collider2D[] placeHolders;
-	public GameObject stageObject;
-	private Partner?[] placement;
-	private GameObject[] placementObject;
+/**
+ * Pallet Picker.
+ * Controls flow of pallect behaviour. pickup & put partner.
+ */
+namespace SpuitFoon
+{
+	public class PalletPicker : MonoBehaviour
+	{
+		public Team team = Team.Player1;
+		public Collider2D[] placeHolders;
+		public GameObject stageObject;
+		private Partner?[] placement;
+		private GameObject[] placementObject;
 
-	// Use this for initialization
-	void Start () {
-		placement = new Partner?[placeHolders.Length];
-		placementObject = new GameObject[placeHolders.Length];
+		// Use this for initialization
+		void Start ()
+		{
+			placement = new Partner?[placeHolders.Length];
+			placementObject = new GameObject[placeHolders.Length];
 
-		for (int i = 0; i < placeHolders.Length; i++) {
-			int index = i;
-			placement[index] = null;
+			for (int i = 0; i < placeHolders.Length; i++) {
+				int index = i;
+				placement [index] = null;
 
-			// mouseDown FIXME: change condition to sprite collision
-			this.UpdateAsObservable ()
-				.Where (_ => Input.GetMouseButtonDown (0))
-				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition))
-				.Select (point => Physics2D.OverlapPoint (point))
-				.Where (collider => collider == placeHolders[index])
-				.Where (_ => PickerManager.Instance.CanPickupPartner())
-				.Where (_ => placement[index] != null)
-				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition))
-				.Select (p => new Vector3(p.x, p.y, stageObject.transform.position.z - 1))
-				.Subscribe(position => {
-						Partner partner = (Partner)placement[index];
-						GameObject partnerObject = (GameObject)placementObject[index];
-						partnerObject.transform.parent = null;
-						PickerManager.Instance.PickupPartner (partner, partnerObject, team, position);
+				// mouseDown FIXME: change condition to sprite collision
+				this.OnMouseDownCollider2DAsObservale ()
+					.Do (collider => Debug.Log ("collider: " + collider))
+					.Where (collider => collider == placeHolders [index] && PickerManager.Instance.CanPickupPartner () && placement [index] != null)
+					.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition))
+					.Select (p => new Vector3 (p.x, p.y, stageObject.transform.position.z - 1))
+					.Subscribe (position => {
+					Partner partner = (Partner)placement [index];
+					GameObject partnerObject = (GameObject)placementObject [index];
+					partnerObject.transform.parent = null;
+					PickerManager.Instance.PickupPartner (partner, partnerObject, team, position);
 
-						placement[index] = null;
-						placementObject[index] = null;
+					placement [index] = null;
+					placementObject [index] = null;
 				});
+			}
+
+			// mouse move
+			this.OnMouseMoveWorldPointAsObservable ()
+				.Select (p => new Vector3 (p.x, p.y, stageObject.transform.position.z - 1))
+				.Subscribe (position => {
+				PickerManager.Instance.selectedPartnerObject.transform.position = position;
+			});
+
+			// mouseUp
+			this.OnMouseUpWorldPointAsObservable ()
+				.Select (p => new Vector3 (p.x, p.y, stageObject.transform.position.z - 1)) // TODO: summerize
+				.Subscribe (position => {
+				if (PickerManager.Instance.PutdownPartner (new Vector2 (position.x, position.y))) {
+					UpdatePicker ();
+				} else {
+					// place back original position.
+				}
+			});
+
+			UpdatePicker ();
 		}
 
-		// mouse move
-		this.UpdateAsObservable ()
-				.Where (_ => PickerManager.Instance.CanPutdownPartner())
-				.Where (_ => !Input.GetMouseButtonDown (0) && !Input.GetMouseButtonUp (0) && Input.GetMouseButton (0))
-				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition))
-				.Select (p => new Vector3(p.x, p.y, stageObject.transform.position.z - 1))
-				.Subscribe (position => {
-					PickerManager.Instance.selectedPartnerObject.transform.position = position;
-				});
+		public void UpdatePicker ()
+		{
+			System.Random random = new System.Random ();
 
-		// mouseUp
-		this.UpdateAsObservable ()
-				.Where (_ => Input.GetMouseButtonUp (0))
-				.Where (_ => PickerManager.Instance.CanPutdownPartner())
-				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition))
-				.Select (p => new Vector3(p.x, p.y, stageObject.transform.position.z - 1)) // TODO: summerize
-				.Subscribe(position => {
-					if (PickerManager.Instance.PutdownPartner (new Vector2(position.x, position.y))) {
-						UpdatePicker ();
-					}
-					else {
-						// place back original position.
-					}
-				});
-
-		UpdatePicker ();
-	}
-
-	public void UpdatePicker() {
-		System.Random random = new System.Random ();
-
-		for (int i = 0; i < placeHolders.Length; i++) {
-			if (placement[i] == null) {
-				Partner partner = PartnerExt.Random ();
-				string path = partner.SpritePath(team);
-				Vector3 position = this.placeHolders[i].transform.position + new Vector3(0,0,-1);
-				placement[i] = partner;
-				placementObject[i] = (GameObject)Instantiate(partner.LoadPartner (team), position, Quaternion.identity);
-				placementObject[i].transform.parent = placeHolders[i].transform;
+			for (int i = 0; i < placeHolders.Length; i++) {
+				if (placement [i] == null) {
+					Partner partner = PartnerExt.Random ();
+					string path = partner.SpritePath (team);
+					Vector3 holderPosition = placeHolders [i].transform.position; 
+					Vector3 position = new Vector3 (holderPosition.x, holderPosition.y, stageObject.transform.position.z);
+					placement [i] = partner;
+					placementObject [i] = (GameObject)Instantiate (partner.LoadPartner (team), position, Quaternion.identity);
+				}
 			}
 		}
 	}
---
+++ Assets/Resources/Scripts/Main/ViewModel/TouchViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace ViewModel
+{
+	public static class GameObjectExtensions
+	{
+		public static IObservable<Collider2D> OnMouseDownCollider2DAsObservale (this MonoBehaviour monoBehaviour)
+		{
+			return monoBehaviour
+				.UpdateAsObservable ()
+				.Where (_ => Input.GetMouseButtonDown (0))
+				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition))
+				.Select (point => Physics2D.OverlapPoint (point));
+		}
+
+		public static IObservable<Vector3> OnMouseMoveWorldPointAsObservable (this MonoBehaviour monoBehaviour)
+		{
+			return monoBehaviour
+				.UpdateAsObservable ()
+				.Where (_ => PickerManager.Instance.CanPutdownPartner ())
+				.Where (_ => !Input.GetMouseButtonDown (0) && !Input.GetMouseButtonUp (0) && Input.GetMouseButton (0))
+				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition));
+		}
+
+		public static IObservable<Vector3> OnMouseUpWorldPointAsObservable (this MonoBehaviour monoBehaviour)
+		{
+			return monoBehaviour
+				.UpdateAsObservable ()
+				.Where (_ => Input.GetMouseButtonUp (0))
+				.Where (_ => PickerManager.Instance.CanPutdownPartner ())
+				.Select (_ => Camera.main.ScreenToWorldPoint (Input.mousePosition));
+		}
+	}
+}
